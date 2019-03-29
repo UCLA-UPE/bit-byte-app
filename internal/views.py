9193e1b9ace6cf97ff950a54cf523f42c3e8be54
Michael Liu <mike666234@gmail.com>
2019-03-29 10:28:09 -0700

    Add minimal view example

--- internal/views.py
+++ internal/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from internal.models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    profiles = Profile.objects.all()
+    context = {'profiles': profiles}
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'internal/index.html', context)
+
+def register(request):
+    # return HttpResponse("Hello, world. You're at the sdsdd index.")
+    # return render(request, 'polls/index.html', context)
+    return render(request, 'bit-byte-app/register.html')
