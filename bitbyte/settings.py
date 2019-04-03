16dc4c01e90eb09b20253343965dd230ae71400a
Alex Hunsader <alex@hunsader.com>
2019-04-03 01:37:58 +0000

    password reset and change

--- bitbyte/settings.py
+++ bitbyte/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'tmp/email')
+
--- internal/urls.py
+++ internal/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -11,5 +12,12 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
     path('teams/', views.teams_view, name='teams'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="internal/password_reset.html"), name="password_reset"),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name="internal/password_reset_done.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="internal/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="internal/password_reset_complete.html"), name="password_reset_complete"),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name="internal/password_change.html"), name="password_change"),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name="internal/password_change_done.html"), name="password_change_done"),
+
     # path('edit/', views.edit, name='edit'),
 ]
