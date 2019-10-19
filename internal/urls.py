9be24339eada05b16536f476fb4625a630bb5ec0
Michael Liu <mike666234@gmail.com>
2019-10-18 22:21:48 -0700

    Prepend bitbyte_ to url names

--- internal/urls.py
+++ internal/urls.py
@@ -1,27 +1,47 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, include
+from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+# from django.shortcuts import redirect
+from django.views.generic.base import RedirectView
+from challenge import urls
 
 from . import views
 
 urlpatterns = [
     path('', views.index_view, name='index'),
-    path('register/', views.register_view, name='register'),
-    path('register/submit/', views.register_submit_view, name='register_submit'),
-    path('login/', views.login_view, name='login'),
-    path('login/submit/', views.login_submit_view, name='login_submit'),
-    path('logout/', views.logout_view, name='logout'),
-    path('profile/', views.profile_view, name='profile'),
-    path('teams/', views.teams_view, name='teams'),
-    path('teams/create/', views.teams_create_view, name='teams_create'),
-    path('teams/join/', views.teams_join_view, name='teams_join'),
+
+    # auth / admin
+    path('register/', views.register_view, name='bitbyte_register'),
+    path('register/submit/', views.register_submit_view, name='bitbyte_register_submit'),
+    path('login/', views.login_view, name='bitbyte_login'),
+    path('login/submit/', views.login_submit_view, name='bitbyte_login_submit'),
+    path('logout/', views.logout_view, name='bitbyte_logout'),
+    path('editprofile/', views.edit_profile_view, name='bitbyte_edit_profile'),
+
+    # builtin auth
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name="internal/password_reset.html"), name="password_reset"),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name="internal/password_reset_done.html"), name="password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="internal/password_reset_confirm.html"), name="password_reset_confirm"),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="internal/password_reset_complete.html"), name="password_reset_complete"),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name="internal/password_change.html"), name="password_change"),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name="internal/password_change_done.html"), name="password_change_done"),
-    path('events/', views.events_view, name='events'),
-    path('events/submit/', views.events_submit_view, name='events_submit'),
-    path('editprofile/', views.edit_profile_view, name='edit_profile')
+
+    # bit-byte app
+    path('profile/', views.profile_view, name='bitbyte_profile'),
+    path('teams/', views.teams_view, name='bitbyte_teams'),
+    path('teams/create/', views.teams_create_view, name='bitbyte_teams_create'),
+    path('teams/join/', views.teams_join_view, name='bitbyte_teams_join'),
+    path('events/', views.events_view, name='bitbyte_events'),
+    path('events/submit/', views.events_submit_view, name='bitbyte_events_submit'),
     # path('edit/', views.edit, name='edit'),
-]
+
+    # # wiki
+    # path('admin/', admin.site.urls),
+    # # path('notifications/', include('django_nyt.urls')),
+    # # path('challenge/', include('wiki.urls')),
+    # path('challenge/', include('challenge.urls')),
+    # path('accounts/login/', RedirectView.as_view(url='/login/')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
